df8db3ff41138c2097345e4bdd6514dc0545e807
ryanlacroix <ryan.lacroix53@gmail.com>
2020-04-26 18:50:04 -0400

    Added new TEI conversion script

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 var path = require('path');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // Accomodate for local and remote deployment
 let port = process.env.PORT;
 if (port == null | port == "") {
-    port = 8000
+    port = 8000;
 }
 
-/*app.defaultConfiguration('*.css', (req, res) => {
-    // Add code to grab query and get file
-})*/
-
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Digital Edition (exhibit page)
 app.get('/', (req, res) => {
-    console.log(req)
-    res.sendFile(path.join(__dirname, './public', 'home.html')) 
+    console.log(req);
+    res.sendFile(path.join(__dirname, './public', 'home.html'));
 })
 
 app.get('*', function(req, res){
     res.status(404).sendFile(path.join(__dirname, './public', '404.html'), 404);
 });
 
-app.listen(port, () => console.log(`kt's website running on port ${port}`))
+app.listen(port, () => console.log(`Site running on port ${port}.`));
--- public/js/manuscript.js
+++ public/js/manuscript.js
@@ -1,27 +1,25 @@
 
 /* Controls flipping through the manuscript exhibit page. */
 
-var currPage = 8;
-var currLetter = 'R'
+var currPage = 7;
+var currLetter = 'R';
 
 window.onload = function() { currPage -= 0.5; currLetter = (currLetter == 'R' ? 'V' : 'R');  this.nextPage() };
 
 function nextPage() {
-    currPage += 0.5
-    currLetter = (currLetter == 'R' ? 'V' : 'R')
-    reqFile = 'ARC_manuscript_1_'+String(Math.floor(currPage))+ currLetter 
-    console.log(reqFile)
-    replaceContent(reqFile)
+    currPage += 0.5;
+    currLetter = (currLetter == 'R' ? 'V' : 'R');
+    reqFile = 'ARC_manuscript_1_'+String(Math.floor(currPage))+ currLetter;
+    console.log(reqFile);
+    replaceContent(reqFile);
 }
 function prevPage() {
-    currPage -= 0.5
-    currLetter = (currLetter == 'R' ? 'V' : 'R')
-    reqFile = 'ARC_manuscript_1_'+String(Math.floor(currPage))+ currLetter
-    console.log(reqFile)
-    replaceContent(reqFile)
+    currPage -= 0.5;
+    currLetter = (currLetter == 'R' ? 'V' : 'R');
+    reqFile = 'ARC_manuscript_1_'+String(Math.floor(currPage))+ currLetter;
+    console.log(reqFile);
+    replaceContent(reqFile);
 }
-
-//ARC_manuscript_1_11R.JPG
 
 function replaceContent(reqFile) {
     // I'im sticking to the basic swap for now. Might implement carousel
@@ -31,7 +29,7 @@ function replaceContent(reqFile) {
         $('[data-toggle="tooltip"]').tooltip();
     });
 
-    // Trying a fancy fade in and out
+    // Fancy fade in and out
     /*$("#manu-img").fadeOut("slow",function(){
         console.log('faded out')
         $("#manu-img").on('load', function () { //avoiding blinking, wait until loaded
